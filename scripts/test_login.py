1abc1727d7db0734c8f96cb35d6ab43f0b4ce06e
zouyan1020 <418747267@qq.com>
2019-10-09 11:57:29 +0800

    zouyan1020

---
+++ scripts/test_login.py
@@ -0,0 +1,13 @@
+class TestLogin:
+
+    def test_login_0(self):
+        assert 1
+
+    def test_login_1(self):
+        assert 1
+
+    def test_login_2(self):
+        assert 0
+
+    def test_login_3(self):
+        assert 0
